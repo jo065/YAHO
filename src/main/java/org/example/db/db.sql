b09611cb5d8c28c4d9374853b67867c97de48a37
jo065 <joeun065@gmail.com>
2023-11-03 12:49:14 +0900

    sql수정

--- src/main/java/org/example/db/db.sql
+++ src/main/java/org/example/db/db.sql
@@ -3,36 +3,31 @@ CREATE DATABASE `yaho`;
 USE `yaho`;
 
 CREATE TABLE `mountain` (
-                            id int UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
-                            name char(100) NOT NULL UNIQUE,
-                            features text NOT NULL,
-                            course text NOT NULL
+    id int UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    name char(100) NOT NULL UNIQUE,
+    features text NOT NULL,
+    course text NOT NULL
 );
-
-INSERT INTO `mountain`
-SET name = '청계산',
-features = '청계산 좋아요',
-course = '잘나옴';
-
-INSERT INTO `mountain`
-SET name = '불장산',
-features = '확인2',
-course = '잘나옴2';
-
-DROP table `mountain`;
-DELETE FROM `mountain`;
-DELETE FROM `mountain` where name = '불장산';
-
-SELECT * FROM `mountain`;
 
 CREATE TABLE `member`(
-                         id int UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
-                         userId char(100) NOT NULL UNIQUE,
-                         `password` char(100) NOT NULL,
-                         nowDateTime datetime NOT NULL,
-                         gender char(100) NOT null
+    id int UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    userId char(100) NOT NULL UNIQUE,
+    `password` char(100) NOT NULL,
+    nowDateTime datetime NOT NULL,
+    gender char(100) NOT null
 );
 
+CREATE TABLE `review`(
+    id int UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    title text NOT NULL,
+    content text NOT NULL,
+    memberId char(100) NOT NULL,
+    regDate DATETIME NOT NULL,
+    score int UNSIGNED NOT NULL
+);
+
+
+--회원
 INSERT INTO `member`
 SET nowDateTime = NOW(),
 userId = '경현잉',
@@ -51,26 +46,13 @@ userId = '조은잉',
 `password` = '11',
 gender = '여자';
 
-CREATE TABLE `review`(
-                         id int UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
-                         title text NOT NULL,
-                         content text NOT NULL,
-                         memberId char(100) NOT NULL,
-                         regDate DATETIME NOT NULL,
-                         score int UNSIGNED NOT NULL
-);
+--산
+INSERT INTO `mountain`
+SET name = '청계산',
+features = '청계산 좋아요',
+course = '잘나옴';
 
-DROP TABLE `review`;
-
-
-SELECT * FROM `member`;
-DESC `member`;
-
-DELETE FROM `member` WHERE id = 6;
-DROP TABLE `member`;
-
-SELECT * FROM `mountain`;
-DESC `mountain`;
-
-SELECT * FROM `review`;
-DESC `review`;
+INSERT INTO `mountain`
+SET name = '불장산',
+features = '확인2',
+course = '잘나옴2';
