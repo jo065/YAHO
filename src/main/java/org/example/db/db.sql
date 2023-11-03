9fa0a3784726fc2447cb438552df5124533f9b3e
jo065 <joeun065@gmail.com>
2023-11-03 12:03:33 +0900

    readmee생성

--- src/main/java/org/example/db/db.sql
+++ src/main/java/org/example/db/db.sql
@@ -3,10 +3,25 @@ CREATE DATABASE `yaho`;
 USE `yaho`;
 
 CREATE TABLE `mountain` (
-                            id int UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
-                            name char(100) NOT NULL UNIQUE,
-                            features text NOT NULL,
-                            course text NOT NULL
+    id int UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    name char(100) NOT NULL UNIQUE,
+    features text NOT NULL,
+    course text NOT NULL
+);
+
+CREATE TABLE `member`(
+    id int UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    userId char(100) NOT NULL UNIQUE,
+    `password` char(100) NOT NULL,
+    nowDateTime datetime NOT NULL,
+    gender char(100) NOT null
+);
+
+CREATE TABLE `review`(
+    id int UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    title text NOT NULL,
+    content text NOT NULL,
+    userId char(100) NOT NULL UNIQUE
 );
 
 INSERT INTO `mountain`
@@ -19,19 +34,6 @@ SET name = '불장산',
 features = '확인2',
 course = '잘나옴2';
 
-DROP table `mountain`;
-DELETE FROM `mountain`;
-DELETE FROM `mountain` where name = '불장산';
-
-SELECT * FROM `mountain`;
-
-CREATE TABLE `member`(
-                         id int UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
-                         userId char(100) NOT NULL UNIQUE,
-                         `password` char(100) NOT NULL,
-                         nowDateTime datetime NOT NULL,
-                         gender char(100) NOT null
-);
 
 INSERT INTO `member`
 SET nowDateTime = NOW(),
@@ -50,22 +52,3 @@ SET nowDateTime = NOW(),
 userId = '조은잉',
 `password` = '11',
 gender = '여자';
-
-CREATE TABLE `review`(
-                         id int UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
-                         title text NOT NULL,
-                         content text NOT NULL,
-                         userId char(100) NOT NULL UNIQUE
-);
-
-SELECT * FROM `member`;
-DESC `member`;
-
-DELETE FROM `member` WHERE id = 6;
-DROP TABLE `member`;
-
-SELECT * FROM `mountain`;
-DESC `mountain`;
-
-SELECT * FROM `review`;
-DESC `review`;
